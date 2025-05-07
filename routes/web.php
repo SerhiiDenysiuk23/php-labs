39294e0b9904c8c1296bb3de5b9389b92f76d391
Serhii Denysiuk <serhiidenysiuk22@gmail.com>
2025-05-08 00:17:55 +0300

    Completed lab2 laravel

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\TestController;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,3 +10,9 @@ Route::get('/', function () {
 });
 
 Route::get('/test', [TestController::class, 'test']);
+
+Route::get('/products', [ProductController::class, 'getProducts']);
+Route::get('/products/{id}', [ProductController::class, 'getProductItem']);
+Route::post('/products', [ProductController::class, 'createProduct'])->withoutMiddleware([VerifyCsrfToken::class]);
+Route::patch('/products/{id}', [ProductController::class, 'updateProduct'])->withoutMiddleware([VerifyCsrfToken::class]);
+Route::delete('/products/{id}', [ProductController::class, 'deleteProduct'])->withoutMiddleware([VerifyCsrfToken::class]);
